2606455931924d3f1d0e2bd2c5290e3b837e8dea
DelPlop <github@phpblocnote.net>
2021-07-26 18:55:13 +0200

    pagination d'utilisateur

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -8,6 +8,7 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 
 /**
  * @method ApplicationUser|null find($id, $lockMode = null, $lockVersion = null)
@@ -17,9 +18,29 @@ use Symfony\Component\Security\Core\User\UserInterface;
  */
 class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
 {
+    public const ITEM_PER_PAGE = 50;
+
     public function __construct(ManagerRegistry $registry, string $class = ApplicationUser::class)
     {
         parent::__construct($registry, $class);
+    }
+
+    public function getPaginator(int $offset, array $criteria = []): Paginator
+    {
+        $qb = $this->createQueryBuilder('u');
+        foreach ($criteria as $criterionName => $criterionValue) {
+            if ($criterionValue === null) {
+                $qb->andWhere('u.' . $criterionName . ' IS NULL');
+            } else {
+                $qb->andWhere('u.' . $criterionName . ' = :crit')
+                    ->setParameter('crit', $criterionValue);
+            }
+        }
+        $qb->orderBy('u.id', 'ASC')
+            ->setMaxResults(self::ITEM_PER_PAGE)
+            ->setFirstResult($offset);
+
+        return new Paginator($qb->getQuery());
     }
 
     /**
